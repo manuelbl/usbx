9ea3f7057682aeb197368793a4094c7840d302d9
Manuel Bl <manuel.bleichenbacher@gmail.com>
2025-02-23 10:12:16 +0100

    Improved process architecture detection

--- src/usbx/usb.py
+++ src/usbx/usb.py
@@ -3,23 +3,29 @@
 # Licensed under MIT License
 # https://opensource.org/licenses/MIT
 
-import ctypes
 import platform
+import os
 
 from .registry import DeviceRegistry
 
-# Import the device registry suitable for the current platform
+# Determine architecture of current process / Python interpreter
 _os = platform.system()
-_arch = platform.machine()
-_bits = ctypes.sizeof(ctypes.c_void_p) * 8
-if _os == 'Darwin' and _arch in ['arm64', 'x86_64'] and _bits == 64:
+if _os == 'Darwin':
+    _os = 'macOS'
+if _os == 'Windows':
+    _arch = os.getenv('PROCESSOR_ARCHITECTURE')
+else:
+    _arch = platform.machine()
+
+# Import the device registry suitable for the current platform
+if _os == 'macOS' and _arch in ['arm64', 'x86_64']:
     from ._macos.macosregistry import MacosDeviceRegistry as NativeDeviceRegistry
-elif _os == 'Linux' and _arch in ['x86_64', 'aarch64'] and _bits == 64:
+elif _os == 'Linux' and _arch in ['x86_64', 'aarch64']:
     from ._linux.linuxregistry import LinuxDeviceRegistry as NativeDeviceRegistry
-elif _os == 'Windows' and _arch in ['AMD64', 'ARM64'] and _bits == 64:
+elif _os == 'Windows' and _arch in ['AMD64', 'ARM64']:
     from ._windows.winregistry import WindowsDeviceRegistry as NativeDeviceRegistry
 else:
-    raise NotImplementedError(f'The library "usbx" is not supported on platform {_os}/{_arch} ({_bits} bit Python interpreter)')
+    raise NotImplementedError(f'The library "usbx" does not support Python interpreter for {_os}/{_arch}')
 
 usb: DeviceRegistry = NativeDeviceRegistry()
 """
